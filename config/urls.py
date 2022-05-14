99dd1f6da38f17b213897b062b3bb11322429708
sevbo2003 <sevbofx@gmail.com>
2022-05-14 21:05:00 +0500

    For deploy

--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('', Home.as_view())
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
